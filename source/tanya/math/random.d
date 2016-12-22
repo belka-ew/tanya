f7fb89fed0a978ba03acf5ad028b2a460afded64
Eugen Wissner <belka@caraus.de>
2016-12-22 21:50:33 +0100

    Move random.d into math submodule

--- source/tanya/math/random.d
+++ source/tanya/math/random.d
@@ -3,12 +3,14 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /**
+ * Random number generator.
+ *
  * Copyright: Eugene Wissner 2016.
  * License: $(LINK2 https://www.mozilla.org/en-US/MPL/2.0/,
  *                  Mozilla Public License, v. 2.0).
  * Authors: $(LINK2 mailto:belka@caraus.de, Eugene Wissner)
  */  
-module tanya.random;
+module tanya.math.random;
 
 import std.experimental.allocator;
 import std.digest.sha;
