5e16fe98d6d36c97e46809169ba16d03a0f72e75
Eugen Wissner <belka@caraus.de>
2017-04-01 09:53:59 +0200

    Add tanya.network package file

---
+++ source/tanya/network/package.d
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Network programming.
+ *
+ * Copyright: Eugene Wissner 2016-2017.
+ * License: $(LINK2 https://www.mozilla.org/en-US/MPL/2.0/,
+ *                  Mozilla Public License, v. 2.0).
+ * Authors: $(LINK2 mailto:info@caraus.de, Eugene Wissner)
+ */
+module tanya.network;
+
+public import tanya.network.socket;
+public import tanya.network.url;
